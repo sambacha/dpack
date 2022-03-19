64d4bc9bfb06399cc2de0068bf126de7eac42bc4
sam bacha <sam@manifoldfinance.com>
2022-03-19 02:47:48 -0700

    build(rollup): test buildout

--- dist/src/dapp.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Dapp {
-    _ethers: any;
-    _pack: any;
-    _objects: any;
-    _types: any;
-    weth: any;
-    private constructor();
-    static loadFromPack(pack: any, _ethers?: any, _signer?: any): Promise<Dapp>;
-}
